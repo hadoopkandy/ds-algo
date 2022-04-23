38f66dae26fb74bc0af95a5b6ae5e6db4ba9905d
kandy01.wang <kandy01.wang@vipshop.com>
2022-04-23 20:27:28 +0800

    三数之和

--- src/main/java/com/kandy/algorithm/week02/LC15三数之和.java
+++ src/main/java/com/kandy/algorithm/week02/LC15三数之和.java
@@ -7,59 +7,68 @@ import java.util.List;
 /**
  * 15. 三数之和
  * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
- *
+ * <p>
  * 注意：答案中不可以包含重复的三元组。
- *
+ * <p>
  *  
- *
+ * <p>
  * 示例 1：
- *
+ * <p>
  * 输入：nums = [-1,0,1,2,-1,-4]
  * 输出：[[-1,-1,2],[-1,0,1]]
  * 示例 2：
- *
+ * <p>
  * 输入：nums = []
  * 输出：[]
  * 示例 3：
- *
+ * <p>
  * 输入：nums = [0]
  * 输出：[]
  */
 public class LC15三数之和 {
     public static List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3)
+            return new ArrayList<>();
+
         int n = nums.length;
         Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<List<Integer>> ans = new ArrayList<>();
         // 枚举 a
-        for (int first = 0; first < n; ++first) {
-            // 需要和上一次枚举的数不相同
-            if (first > 0 && nums[first] == nums[first - 1]) {
+        for (int i = 0; i < n; i++) {
+            //已经排序好，所以后面不可能有三个数加和等于0，直接返回结果
+            if (nums[i] > 0) {
+                return ans;
+            }
+            // 对于重复元素：跳过，避免出现重复解
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            // c 对应的指针初始指向数组的最右端
-            int third = n - 1;
-            int target = -nums[first];
-            // 枚举 b
-            for (int second = first + 1; second < n; ++second) {
-                // 需要和上一次枚举的数不相同
-                if (second > first + 1 && nums[second] == nums[second - 1]) {
-                    continue;
-                }
-                // 需要保证 b 的指针在 c 的指针的左侧
-                while (second < third && nums[second] + nums[third] > target) {
-                    --third;
-                }
-                // 如果指针重合，随着 b 后续的增加
-                // 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
-                if (second == third) {
-                    break;
-                }
-                if (nums[second] + nums[third] == target) {
-                    List<Integer> list = new ArrayList<Integer>();
-                    list.add(nums[first]);
-                    list.add(nums[second]);
-                    list.add(nums[third]);
+            int target = -nums[i];
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
                     ans.add(list);
+
+                    //去除重复解 例如输入：[-2,0,0,2,2]
+                    while (left < right && nums[left + 1] == nums[left]) left++;
+                    while (left < right && nums[right - 1] == nums[right]) right--;
+
+                    //答案不唯一：-4 -1 -1 0 1 2
+                    left++;
+                    right--;
+
+                    //若和大于 target，说明 nums[right]太大，right左移
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    //若和小于 target，说明 nums[left]太小,left右移
+                    left++;
                 }
             }
         }
@@ -67,12 +76,8 @@ public class LC15三数之和 {
     }
 
     public static void main(String[] args) {
-        int[] nums  = new int[]{-1,0,1,2,-1,-4};
+        int[] nums = new int[]{-1, 0, 1, 2, -1, -4};
         System.out.println(threeSum(nums));
-
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
-
     }
 
 
