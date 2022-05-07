62d79ec199c240e9a60be52a1bc67877319ab57d
kandy01.wang <kandy01.wang@vipshop.com>
2022-05-07 23:04:25 +0800

    N皇后

---
+++ src/main/java/com/kandy/algorithm/week02/LC51N皇后.java
@@ -0,0 +1,78 @@
+package com.kandy.algorithm.week02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 皇后们的约束条件:
+ * 1.不能同行
+ * 2.不能同列
+ * 3.不能同斜线 （45度和135度角）
+ */
+public class LC51N皇后 {
+    List<List<String>> res = new ArrayList<>();
+
+    public List<List<String>> solveNQueens(int n) {
+        //n*n的棋盘
+        char[][] chessboard = new char[n][n];
+        for (char[] c : chessboard) {
+            Arrays.fill(c, '.');
+        }
+        backTrack(n, 0, chessboard);
+        return res;
+    }
+
+    //n 为输入的棋盘大小
+    //row 是当前递归到棋盘的第几行了
+    //棋盘的宽度就是for循环的长度col，递归的深度就是棋盘的高度row
+    public void backTrack(int n, int row, char[][] chessboard) {
+        if (row == n) {
+            res.add(Array2List(chessboard));
+            return;
+        }
+
+        for (int col = 0; col < n; ++col) {
+            if (isValid(row, col, n, chessboard)) {//验证合法就可以放
+                chessboard[row][col] = 'Q';// 放置皇后
+                backTrack(n, row + 1, chessboard);
+                chessboard[row][col] = '.'; // 回溯，撤销皇后
+            }
+        }
+
+    }
+
+
+    public List Array2List(char[][] chessboard) {
+        List<String> list = new ArrayList<>();
+
+        for (char[] c : chessboard) {
+            list.add(new String(c));
+        }
+        return list;
+    }
+
+    public boolean isValid(int row, int col, int n, char[][] chessboard) {
+        // 检查列
+        for (int i = 0; i < row; ++i) { // 相当于剪枝
+            if (chessboard[i][col] == 'Q') {
+                return false;
+            }
+        }
+
+        // 检查45度对角线是否有皇后
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chessboard[i][j] == 'Q') {
+                return false;
+            }
+        }
+
+        // 检查135度对角线是否有皇后
+        for (int i = row - 1, j = col + 1; i >= 0 && j <= n - 1; i--, j++) {
+            if (chessboard[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
