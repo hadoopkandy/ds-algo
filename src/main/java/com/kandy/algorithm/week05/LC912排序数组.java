cd839bab084585aabab5476e8e5ac0ebf1296f6b
kandy01.wang <kandy01.wang@vipshop.com>
2022-05-16 23:37:18 +0800

    快排

--- src/main/java/com/kandy/algorithm/week05/LC912排序数组.java
+++ src/main/java/com/kandy/algorithm/week05/LC912排序数组.java
@@ -162,6 +162,32 @@ public class LC912排序数组 {
         return r;
     }
 
+    //快速排序
+    public static void _quickSort(int[] list, int low, int high) {
+        if (low < high) {
+            int middle = getMiddle(list, low, high);//将list数组进行一分为二
+            _quickSort(list, low, middle - 1);//对低字表进行递归排序
+            _quickSort(list, middle + 1, high);//对高字表进行递归排序
+        }
+    }
+
+    public static int getMiddle(int[] list, int low, int high) {
+        int tmp = list[low];//数组的第一个作为中轴
+        while (low < high) {
+            while (low < high && list[high] >= tmp) {
+                high--;
+            }
+            list[low] = list[high];//比中轴小的记录移到低端
+            while (low < high && list[low] <= tmp) {
+                low++;
+            }
+            list[high] = list[low];//比中轴大的记录移到高端
+        }
+        list[low] = tmp;//中轴记录到尾
+        return low;//返回中轴的位置
+    }
+
+
     //归并排序 基于分治的算法 O(nlogn) 合并时间复杂度O(n) logn层
     private void mergeSort(int[] nums, int left, int right) { // sort arr[l..r]
         if (left >= right) return;
