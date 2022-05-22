bb33fd9362c108a426bd2fa8c1f2ed01830000cc
kandy01.wang <kandy01.wang@vipshop.com>
2022-05-23 00:09:54 +0800

    sort

---
+++ src/main/java/com/kandy/algorithm/week05/LC242有效的字母异位词.java
@@ -0,0 +1,16 @@
+package com.kandy.algorithm.week05;
+
+import java.util.Arrays;
+
+public class LC242有效的字母异位词 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+}
