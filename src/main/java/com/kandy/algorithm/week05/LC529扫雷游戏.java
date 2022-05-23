5edc3c29000f3f83f8139957b3447820d68ffb08
kandy01.wang <kandy01.wang@vipshop.com>
2022-05-23 19:24:58 +0800

    扫雷游戏题

--- src/main/java/com/kandy/algorithm/week05/LC529扫雷游戏.java
+++ src/main/java/com/kandy/algorithm/week05/LC529扫雷游戏.java
@@ -9,7 +9,7 @@ package com.kandy.algorithm.week05;
  *
  * 解题思路：
  * 1.如果是雷，直接更新成X退出
- * 2.统计当前格子周围雷的数量，如果数量>=1 则更新成累的数量，否则更新成B
+ * 2.统计当前格子周围有雷，则将当前格子更新成雷的数量，否则更新成B
  * 3.如果附近有雷，则不再继续扩展  没有雷 则按照2 递归拓展
  */
 public class LC529扫雷游戏 {
@@ -21,7 +21,7 @@ public class LC529扫雷游戏 {
             // 规则 1 如果一个地雷（'M'）被挖出，游戏就结束了- 把它改为 'X'
             board[x][y] = 'X';
         } else {
-            //进入DFS遍历
+            //游戏继续，进入DFS遍历
             dfs(board, x, y);
         }
         return board;
@@ -30,14 +30,14 @@ public class LC529扫雷游戏 {
     public void dfs(char[][] board, int x, int y) {
         int cnt = 0;
         for (int i = 0; i < 8; ++i) {
-            int tx = x + dx[i];
-            int ty = y + dy[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
             //格子越界 continue
-            if (tx < 0 || tx >= m || ty < 0 || ty >= n) {
+            if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
                 continue;
             }
             // 不用判断 M，因为如果有 M 的话游戏已经结束了
-            if (board[tx][ty] == 'M') {
+            if (board[nx][ny] == 'M') {
                 ++cnt; //统计x y周围有多少雷
             }
         }
@@ -47,15 +47,15 @@ public class LC529扫雷游戏 {
             board[x][y] = (char) (cnt + '0');
         } else {
             // 规则 2 如果当前 x y 周围没有雷，先将当前x y这个位置，更新成B，然后开始递归的搜索和这个x y相邻的雷的情况
-            board[x][y] = 'B';//记忆化搜索
+            board[x][y] = 'B';//记忆化搜索 将 E 改成 B
             for (int i = 0; i < 8; ++i) {
-                int tx = x + dx[i];
-                int ty = y + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 // 这里不需要在存在 B 的时候继续扩展，因为 B 之前被点击的时候已经被扩展过了
-                if (tx < 0 || tx >= m || ty < 0 || ty >= n || board[tx][ty] != 'E') {
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n || board[nx][ny] != 'E') {
                     continue;
                 }
-                dfs(board, tx, ty);
+                dfs(board, nx, ny);
             }
             //影藏的递归终止条件2：没有更多的方块可揭露（要么越界，要么都已经E都已经访问过（变成B））
         }
