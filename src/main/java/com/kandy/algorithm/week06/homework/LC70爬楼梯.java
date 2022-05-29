0f3d0d8f3d3565d513dcfa8b0da76b606d09f67f
kandy01.wang <kandy01.wang@vipshop.com>
2022-05-30 00:09:23 +0800

    comment

--- src/main/java/com/kandy/algorithm/week06/homework/LC70爬楼梯.java
+++ src/main/java/com/kandy/algorithm/week06/homework/LC70爬楼梯.java
@@ -10,7 +10,7 @@ public class LC70爬楼梯 {
         for (int i = 1; i <= n; i++) {
             if (i == 1) { f[i] = 1; continue; } //n=1 1种方法
             if (i == 2) { f[i] = 2; continue; } //n=2 2种方法
-            f[i] = f[i - 1] + f[i - 2]; //状态转移方法
+            f[i] = f[i - 1] + f[i - 2]; //状态转移方程
         }
         return f[n];
     }
