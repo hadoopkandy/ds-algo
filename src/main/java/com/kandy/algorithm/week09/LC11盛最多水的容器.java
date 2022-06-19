bba0e65c75e3db3a27254585a7d502925185e375
kandy01.wang <kandy01.wang@vipshop.com>
2022-06-19 16:07:09 +0800

    盛最多水的容器 双指针

--- src/main/java/com/kandy/algorithm/week09/LC11盛最多水的容器.java
+++ src/main/java/com/kandy/algorithm/week09/LC11盛最多水的容器.java
@@ -1,6 +1,10 @@
 package com.kandy.algorithm.week09;
 
 public class LC11盛最多水的容器 {
+    //解题步骤：
+    //1.两重循环枚举，找冗余
+    //2.发现关键，盛多少水是由短的那一根决定的，短的算完就没用了
+    //3.双指针，两个指针从头到尾向中间移动，每次移动短的那根
     public int maxArea(int[] height) {
         int i = 0, j = height.length - 1;
         int ans = 0;
