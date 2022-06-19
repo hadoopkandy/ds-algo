47d366f19ff17b11d59cc686c23b17ee93e3aed6
kandy01.wang <kandy01.wang@vipshop.com>
2022-06-19 16:04:30 +0800

    盛最多水的容器 双指针

--- src/main/java/com/kandy/algorithm/week09/LC11盛最多水的容器.java
+++ src/main/java/com/kandy/algorithm/week09/LC11盛最多水的容器.java
@@ -5,9 +5,9 @@ public class LC11盛最多水的容器 {
         int i = 0, j = height.length - 1;
         int ans = 0;
         while (i < j) {
-            //高度较小者* 宽度，更新答案
+            //高度较小者* 宽度j-i，更新答案
             ans = Math.max(ans, Math.min(height[i], height[j]) * (j - i));
-            //谁短，谁就没用了，就缩谁
+            //谁小，谁就没用了，就缩谁
             if (height[i] < height[j]) i++; else j--;
         }
         return ans;
