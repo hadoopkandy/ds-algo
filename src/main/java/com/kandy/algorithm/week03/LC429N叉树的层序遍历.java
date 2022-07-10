01b551326129434227b258487560de911d6eba29
kandy01.wang <kandy01.wang@vipshop.com>
2022-07-10 20:45:06 +0800

    BFS

--- src/main/java/com/kandy/algorithm/week03/LC429N叉树的层序遍历.java
+++ src/main/java/com/kandy/algorithm/week03/LC429N叉树的层序遍历.java
@@ -14,12 +14,13 @@ import java.util.Queue;
  */
 public class LC429N叉树的层序遍历 {
 
+    //queue = [<1,0>,<3,1>,<2,1>,<4,1>,<5,2>,<6,2>]
     public List<List<Integer>> levelOrder(Node root) {
         //<节点，深度从0开始>
         Queue<Pair<Node, Integer>> q = new LinkedList<Pair<Node, Integer>>();
         List<List<Integer>> seq = new ArrayList<List<Integer>>();
         if (root == null) return seq;
-        //放入根节点
+        //放入根节点 <点，层数>
         q.add(new Pair<Node, Integer>(root, 0));
         while (!q.isEmpty()) {
             Node node = q.peek().getKey();
